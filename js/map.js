2798bea7fa994e638af00743a53793d7a480b1c4
Eric Martz <ericmartz@gmail.com>
2015-11-01 20:01:36 -0500

    Added mapMarkers to map

--- js/map.js
+++ js/map.js
@@ -1,49 +1,71 @@
 var mapPoints = [
   {
     location: 'Ormsby\'s Tavern',
-    type: 'restaurant'
+    address: '1170 Howell Mill Rd, Atlanta GA 30318',
+    type: 'restaurant',
+    visible: true
   },
   {
     location: 'Tap',
-    type: 'restaurant'
+    address: '1180 Peachtree St NE, Atlanta, GA 30309',
+    type: 'restaurant',
+    visible: true
   },
   {
     location: 'Rathbun\'s Steakhouse',
-    type: 'restaurant'
+    address: '154 Krog St NE #200, Atlanta, GA 30307',
+    type: 'restaurant',
+    visible: true
   },
   {
     location: 'Houston\'s',
-    type: 'restaurant'
+    address: '2166 Peachtree Road NW, Atlanta, GA 30309',
+    type: 'restaurant',
+    visible: true
   },
   {
     location: 'The Vortex',
-    type: 'restaurant'
+    address: '878 Peachtree St NE, Atlanta, GA 30309',
+    type: 'restaurant',
+    visible: true
   },
   {
-    location: 'Coke Museum',
-    type: 'site'
+    location: 'World of Coca Cola',
+    address: '121 Baker St NW, Atlanta, GA 30313',
+    type: 'site',
+    visible: true
   },
   {
     location: 'Atlanta Zoo',
-    type: 'site'
+    address: '800 Cherokee Ave SE, Atlanta, GA 30315',
+    type: 'site',
+    visible: true
   },
   {
     location: 'Little Five Points',
-    type: 'site'
+    address: 'Little Five Points, Atlanta, GA',
+    type: 'site',
+    visible: true
   },
   {
     location: 'The Fox Theater',
-    type: 'site'
+    address: '660 Peachtree St NE, Atlanta, GA 30308',
+    type: 'site',
+    visible: true
   },
   {
     location: 'High Museum of Art',
-    type: 'site'
+    address: '1280 Peachtree St NE, Atlanta, GA 30309',
+    type: 'site',
+    visible: true
   },
 ]
 
 var MapPoint = function(data) {
   this.mapLocation = ko.observable(data.location);
+  this.mapLocationAddress = ko.observable(data.address);
   this.mapLocationType = ko.observable(data.type);
+  this.mapLocationVisible = ko.observable(data.visible);
 }
 
 var ViewModel = function() {
@@ -54,6 +76,9 @@ var ViewModel = function() {
   mapPoints.forEach(function(mapPoint){
     self.mapPointsList.push(new MapPoint(mapPoint));
   });
+
+  pinPoster(this.mapPointsList());
+
 }
 
 ko.applyBindings(new ViewModel());
